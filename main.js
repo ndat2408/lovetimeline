a6ce75d7f74c2c3f0f65b51515749258b3993667
Nguyen The Dat <67305993+ndat2408@users.noreply.github.com>
2023-01-26 20:12:39 +0700

    Update main.js

--- main.js
+++ main.js
@@ -16,5 +16,3 @@ for (let i = 0; i < rigthItems.length; ++i) {
 }
 
 alert("Web này anh chỉ tổng hợp được sương sương hoi ó, tại nhiều mốc anh không có nhớ với cũng không có ảnh :( À anh sắp xếp theo thời gian từ gần nhất đến xa nhất á, nên bé muốn xem từ ngày đầu thì kéo xuống dưới nhó:33 iu bé")
-
-
